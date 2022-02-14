75285d006473d8c86b16e03fe93a1dd1cbe7a103
Djamel Ramdani <enzonpapa@gmail.com>
2022-02-13 18:26:40 -0600

    gh-pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -44,7 +44,7 @@ class Header extends Component {
 
             <li>
               <a className="smoothscroll" href="#contact">
-                Contact
+                Contacts
               </a>
             </li>
           </ul>
